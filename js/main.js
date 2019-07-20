71e1b264e455dc3a701e67bacf9b613a7b2aa82b
mrobin88 <ratthewrobin@gmail.com>
2019-07-20 14:53:53 -0700

    Game logic created

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log('im running.');
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardThree);
