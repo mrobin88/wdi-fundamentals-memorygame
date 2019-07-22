41518b6c59e66360594f717ec1dddf576055b615
mrobin88 <ratthewrobin@gmail.com>
2019-07-22 00:16:57 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
-var cards = ["king","king","queen","queen"];
+var cards = [ 
+  
+    {
+    rank: "queen",
+    suit: "hearts",
+    cardImage:"images/queen-of-hearts.png"
+    },{
+        rank: "queen",
+        suit: "diamonds",
+        cardImage:"images/queen-of-diamonds.png"
+    },{
+        rank: "king"
+        suit: "hearts",
+        cardImage:"images/king-of-hearts.png"
+    },{
+        rank: "king",
+        suit: "diamonds",
+        cardImage:"images/king-of-diamonds.png"
+    }
+];
 
 var cardsInPlay = [];
+//this needs to be a function that says when card is flipped 
+function flipCards() {
+ console.log("User flipped" + cards[cardId].rank);
+}
+cardsInPlay.push(cards.rank);
 
-var cardOne = cards[0];
-
-var cardTwo = cards[3];
-
-cardsInPlay.push(cardOne);
-
-console.log("the user flipped the king");
+console.log("user flipped" + cards.rank + "of" + cards.suit);
 
 console.log("user flipped queen");
-
-cardsInPlay.push(cardTwo);
 
 console.log("user flipped king");
 
 if (cardsInPlay.length === 2){
-    console.log("Yup we got 2");
+console.log("Yup we got 2");
+
+}else if(cardsInPlay[0] === cardsInPlay[1]) {
+
+console.log('alert you got a match');
+
+}else if (cardsInPlay[0] !== cardsInPlay[1]){
+console.log("try again");
 
 }
