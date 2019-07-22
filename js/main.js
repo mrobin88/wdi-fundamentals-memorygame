6c8135b6c5f016451b2a915130daa37379938afb
mrobin88 <ratthewrobin@gmail.com>
2019-07-21 19:07:00 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-console.log('im running.');
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["king","king","queen","queen"];
 
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardThree);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardOne);
+
+console.log("the user flipped the king");
+
+console.log("user flipped queen");
+
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped king");
+
+if (cardsInPlay.length === 2){
+    console.log("Yup we got 2");
+
+}
